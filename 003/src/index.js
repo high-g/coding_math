fb22762891bbd02ecf6f4921a5958f00d915fffa
tani <tani@1-10.com>
2019-04-24 13:30:50 +0900

    [add]canvas 2d circle test

---
+++ 003/src/index.js
@@ -0,0 +1,28 @@
+import './style.scss'
+
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+const width = canvas.width = window.innerWidth
+const height = canvas.height = window.innerHeight
+
+const centerY = height * .5
+const centerX = width * .5
+const baseRadius = 100
+const offset = 50
+const speed = 0.1
+let angle = 0
+
+render()
+
+function render() {
+  // const y = centerY + Math.sin(angle) * offset
+  const radius = baseRadius + Math.sin(angle) * offset
+
+  context.clearRect(0, 0, width, height)
+  context.beginPath()
+  context.arc(centerX, centerY, radius, 0, Math.PI * 2, false)
+  context.fill()
+
+  angle += speed
+  requestAnimationFrame(render)
+}
