32ccb5d343674029436d42ea70676ec513fb1a97
tani <tani@1-10.com>
2019-04-26 19:39:32 +0900

    [update]

--- 003/src/index.js
+++ 003/src/index.js
@@ -8,7 +8,8 @@ const height = canvas.height = window.innerHeight
 const centerY = height * .5
 const centerX = width * .5
 const baseRadius = 100
-const offset = 50
+const baseAlpha = 0.5
+const offset = 0.5
 const speed = 0.1
 let angle = 0
 
@@ -16,11 +17,13 @@ render()
 
 function render() {
   // const y = centerY + Math.sin(angle) * offset
-  const radius = baseRadius + Math.sin(angle) * offset
+  // const radius = baseRadius + Math.sin(angle) * offset
+  const alpha = baseAlpha + Math.sin(angle) * offset
 
+  context.fillStyle = 'rgba(0, 0, 0, ' + alpha + ')'
   context.clearRect(0, 0, width, height)
   context.beginPath()
-  context.arc(centerX, centerY, radius, 0, Math.PI * 2, false)
+  context.arc(centerX, centerY, 100, 0, Math.PI * 2, false)
   context.fill()
 
   angle += speed
